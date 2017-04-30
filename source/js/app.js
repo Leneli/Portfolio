8c43b45717194f7f088a0d74b15c0b7727c08505
Liafell <leneli.lehmann@yahoo.de>
2017-04-30 23:29:57 +0300

    Flip

--- source/js/app.js
+++ source/js/app.js
@@ -1,7 +1,5 @@
 (function() {
 	"use strict";
 
-
 	svg4everybody(); //for SVG-elements in IE
-	console.log("hello APP!");
 })();
---
+++ source/js/flip.js
@@ -0,0 +1,34 @@
+(function() {
+	"use strict";
+
+	var authorize = document.getElementById("authorize"),
+		flipClass = "flipper";
+
+	//login form
+	authorize.addEventListener("click", runFlip);
+
+
+	//flip effect
+	function runFlip() {
+		let elem = document.getElementById(this.dataset.flipId),
+			classes = elem.classList;
+
+		classes.contains(flipClass) === true ? classes.remove(flipClass) : classes.add(flipClass);
+	}
+
+	//перевернуть карту при клике вне ее
+	document.body.addEventListener("click", function(e) {
+		if(!e) e = window.event;
+    	var elems = e.path,
+			flag = true;
+
+		for(let i = 0; i < elems.length; i++) {
+			if(elems[i].id === "authorize" || elems[i].id === "login") {
+				flag = false;	
+			}
+		}
+
+		if(flag) document.getElementById("login").classList.remove(flipClass);
+	});
+
+})();
