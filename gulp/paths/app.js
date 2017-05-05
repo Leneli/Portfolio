6fbc3ef8b3ef124a1730a782f968bc1f7758d2c6
Liafell <leneli.lehmann@yahoo.de>
2017-05-05 22:10:24 +0300

    Parallax

--- gulp/paths/app.js
+++ gulp/paths/app.js
@@ -2,5 +2,6 @@
 
 module.exports = [
 	"./source/js/app.js",
-	"./source/js/flip.js"
+	"./source/js/flip.js",
+	"./source/js/parallax.js"
 ];
---
+++ source/js/parallax.js
@@ -0,0 +1,30 @@
+"use strict";
+
+window.onload = function() {
+	let layers = document.getElementsByClassName("parallax__layer"),
+	parallax;
+
+
+	let moveLaers = function(e) {
+		e = e || event;
+
+		let initialX = (window.innerWidth / 2) - e.pageX,
+			initialY = (window.innerHeight / 2) - e.pageY;
+
+		for(let i = 0; i < layers.length; i++) {
+			let divider;
+
+			if(layers.length >= 5) divider = i / 100;
+			else divider = (i + 1) / 100;
+
+			let X = initialX * divider,
+				Y = initialY * divider,
+				transformStyle = "translate3d(" + X + "px, " + Y + "px, " + "0)";
+
+			layers[i].style.transform = transformStyle;
+		}
+	};
+
+
+	window.addEventListener("mousemove", moveLaers);
+};
