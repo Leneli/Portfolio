452b689da69257c5045217b368412b28b7bf2bb0
Liafell <leneli.lehmann@yahoo.de>
2017-05-11 10:18:15 +0300

    Контакты на странице About

--- source/js/map/google.map.js
+++ source/js/map/google.map.js
@@ -2,7 +2,7 @@
 google.maps.event.addDomListener(window, "load", init);
 
 function init() {
-    var mapOptions = {
+    let mapOptions = {
         center: new google.maps.LatLng(48.821161, 44.621635),
         zoom: 14,
         gestureHandling: "none",
@@ -24,15 +24,19 @@ function init() {
         styles: [{ "featureType": "administrative", "elementType": "labels.text.fill", "stylers": [{ "color": "#444444" }] }, { "featureType": "landscape", "elementType": "all", "stylers": [{ "color": "#f2f2f2" }] }, { "featureType": "poi", "elementType": "all", "stylers": [{ "visibility": "off" }] }, { "featureType": "road", "elementType": "all", "stylers": [{ "saturation": -100 }, { "lightness": 45 }] }, { "featureType": "road.highway", "elementType": "all", "stylers": [{ "visibility": "simplified" }] }, { "featureType": "road.arterial", "elementType": "labels.icon", "stylers": [{ "visibility": "off" }] }, { "featureType": "transit", "elementType": "all", "stylers": [{ "visibility": "off" }] }, { "featureType": "water", "elementType": "all", "stylers": [{ "color": "#99b16a" }, { "visibility": "on" }] }]
     }
 
-    var mapElement = document.getElementById("map");
-    var map = new google.maps.Map(mapElement, mapOptions);
-    var icon = {
+    let mapElement = document.getElementById("map");
+    
+    mapElement.style.height = window.innerHeight + document.querySelector(".footer").offsetHeight + "px";
+    mapElement.style.marginBottom = "-" + document.querySelector(".footer").offsetHeight + "px";
+
+    let map = new google.maps.Map(mapElement, mapOptions);
+    let icon = {
         url: "assets/img/icons-png/map_marker.svg",
         scaledSize: new google.maps.Size(42, 56),
         origin: new google.maps.Point(0, 0),
         anchor: new google.maps.Point(0, 56)
     };
-    var marker = new google.maps.Marker({
+    let marker = new google.maps.Marker({
         position: new google.maps.LatLng(48.822576118823534, 44.6399678251953),
         map: map,
         title: "Елена Куклина - разработка современных сайтов",
