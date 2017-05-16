b6baaee5002b83d76f3e58df01ae1d7f6808724d
Liafell <leneli.lehmann@yahoo.de>
2017-05-16 09:37:51 +0300

    gitignore

--- source/js/parallax.js
+++ source/js/parallax.js
@@ -1,30 +1,29 @@
 //Parallax
-(function() {
-	document.addEventListener("DOMContentLoaded", function() {
-			let layers = document.getElementsByClassName("parallax__layer"),
-	parallax;
+(function () {
+	document.addEventListener("DOMContentLoaded", function () {
+		let layers = document.getElementsByClassName("parallax__layer"),
+			parallax;
 
-	let moveLaers = function(e) {
-		e = e || event;
+		let moveLaers = function (e) {
+			e = e || event;
 
-		let initialX = (window.innerWidth / 2) - e.pageX,
-			initialY = (window.innerHeight / 2) - e.pageY;
+			let initialX = (window.innerWidth / 2) - e.pageX,
+				initialY = (window.innerHeight / 2) - e.pageY;
 
-		for(let i = 0; i < layers.length; i++) {
-			let divider;
+			for (let i = 0; i < layers.length; i++) {
+				let divider;
 
-			if(layers.length >= 5) divider = i / 100;
-			else divider = (i + 1) / 100;
+				if (layers.length >= 5) divider = i / 100;
+				else divider = (i + 1) / 100;
 
-			let X = initialX * divider,
-				Y = initialY * divider,
-				transformStyle = "translate3d(" + X + "px, " + Y + "px, " + "0)";
+				let X = initialX * divider,
+					Y = initialY * divider,
+					transformStyle = "translate3d(" + X + "px, " + Y + "px, " + "0)";
 
-			layers[i].style.transform = transformStyle;
-		}
-	};
+				layers[i].style.transform = transformStyle;
+			}
+		};
 
-	window.addEventListener("mousemove", moveLaers);
+		window.addEventListener("mousemove", moveLaers);
 	});
 })();
-
